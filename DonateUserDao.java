accf3dc6a2d870d67c9b067188644fa236dadf0a
Deepak Nagar <134583380+deepaknagar62@users.noreply.github.com>
2023-06-27 23:43:26 +0530

    Create DonateUserDao.java

---
+++ DonateUserDao.java
@@ -0,0 +1,41 @@
+// this is user Dao who donate blood
+package com.example.bloodbank;
+
+public class Member {
+    private String name;
+    private String phone;
+    private String Blood_Group;
+    private String address;
+
+    Member(){};
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getBlood_Group() { return Blood_Group; }
+
+    public void setBlood_Group(String Blood_Group) {
+        this.Blood_Group = Blood_Group;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+}
